0aae1ee9963219bd5f6caea169413130d9dcb124
Joshua Gottus <125707035+GottusJ@users.noreply.github.com>
2023-07-17 03:18:52 +0200

    Update views.py

--- views.py
+++ views.py
@@ -17,9 +17,28 @@ def prfile():
 
 @views.route("/json")
 def get_json():
-    return jsonify({"name": "Joshua"})
+    return jsonify(
+        {
+            "users": [
+                {"name": "John Doe", "age": 30, "email": "john.doe@example.com"},
+                {"name": "Jane Smith", "age": 25, "email": "jane.smith@example.com"},
+                {
+                    "name": "Michael Johnson",
+                    "age": 35,
+                    "email": "michael.johnson@example.com",
+                },
+            ],
+            "products": [
+                {"name": "Product 1", "price": 19.99, "quantity": 10},
+                {"name": "Product 2", "price": 9.99, "quantity": 5},
+                {"name": "Product 3", "price": 14.99, "quantity": 3},
+            ],
+            "countries": ["USA", "Canada", "UK", "Australia"],
+        }
+    )
 
 
 @views.route("/go-to-home")
 def go_to_home():
     return redirect(url_for("views.home"))
+
